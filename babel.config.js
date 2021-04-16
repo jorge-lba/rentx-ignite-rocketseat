10ce5b156d39a98ec40e5b937574d6f552e2c963
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-04-16 15:41:55 -0300

    Add - Ajustes nos configs do babel

--- babel.config.js
+++ babel.config.js
@@ -5,19 +5,19 @@ module.exports = {
   ],
   plugins: [
     [
-      "model-resolver",
+      "module-resolver",
       {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@errors": ["./src/errors"],
-          "@utils": ["./src/utils"],
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@errors": "./src/errors",
+          "@utils": "./src/utils",
         },
       },
     ],
     "babel-plugin-transform-typescript-metadata",
     ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { lose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
   ],
 };
