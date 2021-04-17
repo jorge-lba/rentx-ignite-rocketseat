34b5dc72eda9228c5bd2744f038b0fb0adc9136e
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-04-17 18:10:37 -0300

    Add - Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -15,6 +16,7 @@ import swaggerFile from "../../../swagger.json";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
